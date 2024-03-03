b47d0d1bc92d53b997cf8889d091917cf4b77f65
DonZiglioni <shift2bass@gmail.com>
2024-03-03 04:29:49 -0500

    Adding imports to Index

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
 import router from './routes/dominoRoutes.js';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const PORT = 8080;
+const uri = process.env.DATABASE_URI;
+const client = new MongoClient(uri);
+const database = client.db('Dominos');
+const boxes = database.collection('Boxes');
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
