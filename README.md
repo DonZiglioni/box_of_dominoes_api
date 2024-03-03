6c3e01c6fed45a339dc83a0bd3fbf7b7d8d7575f
DonZiglioni <shift2bass@gmail.com>
2024-03-03 03:02:37 -0500

    Combining Front and Back

---
+++ README.md
@@ -0,0 +1,7 @@
+# Box of Dominoes API
+***
+This is the repository for the Back-End portion of the Box of Dominoes API
+***
+### Love Life...
+> Life Loves You.
+
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import cors from 'cors';
 import router from './routes/dominoRoutes.js';
 import * as dotenv from 'dotenv';
@@ -8,19 +8,14 @@ const app = express();
 const PORT = 8080;
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }))
-app.use(express.static('public'))
-//app.use(express.static('dist'))
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.static('public'));
+app.use(express.static('dist'));
 
 //  ****  V1  ****
 
-// app.get('/', async (req, res) => {
-//     res.status(200).json({ message: "Hello from Front Page!" })
-// })
-
-app.use('/api/v1/', router)
+app.use('/api/v1/', router);
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT: ", PORT);
-})
+});
